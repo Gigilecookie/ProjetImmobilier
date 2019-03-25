c6fa56d48360ccf664f2f3374c6fba1f78e25048
Gigilecookie <34405939+Gigilecookie@users.noreply.github.com>
2019-03-25 12:31:42 +0100

    attention marche pas

---
+++ ProjetImmobilier/Model/EstateItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetImmobilier.Model
+{
+    class EstateItem
+    {
+        private String title;
+        private String estateType;
+        private String image;
+
+        public EstateItem(String title, String estateType, String image)
+        {
+
+            this.title = title;
+            this.estateType = estateType;
+            this.image = image;
+
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string EstateType
+        {
+            get { return estateType; }
+            set { estateType = value; }
+        }
+
+        public string Image
+        {
+            get { return image; }
+            set { image = value; }
+        }
+    }
+}
--- ProjetImmobilier/ViewModel/EstateViewModel.cs
+++ ProjetImmobilier/ViewModel/EstateViewModel.cs
@@ -1,13 +1,40 @@
-﻿using System;
+﻿using ProjetImmobilier.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ProjetImmobilier.ViewModel
 {
-    class EstateViewModel
+    class EstateViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ObservableCollection<EstateItem> listEstate;
+
+        public EstateViewModel()
+        {
+            listEstate = new ObservableCollection<EstateItem>();
+            listEstate.Add(new EstateItem("Maison 1", "Maison", ""));
+            listEstate.Add(new EstateItem("Appartement 1", "Appartement", ""));
+
+        }
+
+        public ObservableCollection<EstateItem> ListEstate
+        {
+            get { return listEstate; }
+            set
+            {
+                if (value != listEstate)
+                {
+                    listEstate = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ListEstate)));
+                }
+            }
+        }
 
     }
 }
