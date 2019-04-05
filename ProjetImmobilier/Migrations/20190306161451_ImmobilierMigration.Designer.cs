bc9c930210e574ed4b08a7ef0e9a5d9b37a5541f
Hugo Ferrer <hugo-ferrer@hotmail.fr>
2019-04-05 10:49:06 +0200

    BD ok

--- ProjetImmobilier/Migrations/20190306161451_ImmobilierMigration.Designer.cs
+++ ProjetImmobilier/Migrations/20190306161451_ImmobilierMigration.Designer.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using ProjetImmobilier.DataAccess;
+using ProjetImmobilier.Model;
 
 namespace ProjetImmobilier.Migrations
 {
